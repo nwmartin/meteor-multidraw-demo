c1db17540782be1e76368a5e3b0ca4d559131ae9
nwmartin <martin.nick@gmail.com>
2013-04-02 14:03:03 -0400

    Drawing on click with svg.

--- meteor-multidraw-demo.js
+++ meteor-multidraw-demo.js
@@ -1,12 +1,41 @@
 points = new Meteor.Collection('pointsCollection');
 
 if (Meteor.isClient) {
+
+  var svg;
+
   Template.drawingSurface.points = function () {
     return points.find();
   }
 
   Deps.autorun( function () {
     Meteor.subscribe('pointsSubscription');
+  });
+
+  Meteor.startup( function() {
+    svg = d3.select('#canvas').append('svg')
+      .attr('width', '100%')
+      .attr('height', '100%');
+  });
+
+  Deps.autorun( function() {
+    var data = points.find().fetch();
+    if (svg) {
+      svg.selectAll('circle').data(data, function(d) { return d._id; })
+      .enter().append('circle')
+      .attr('r', 10)
+      .attr('cx', function (d) { return d.x; })
+      .attr('cy', function (d) { return d.y; });
+    }
+  });
+
+  Template.drawingSurface.events({
+    'click': function (event) {
+      var offset = $('#canvas').offset();
+      points.insert({
+        x: (event.x - offset.left),
+        y: (event.y - offset.top)});
+    }
   });
 }
 
