b0fec54d94e3d0069fd1fc7317beaf9d61cc3ed0
nwmartin <martin.nick@gmail.com>
2013-04-02 11:41:38 -0400

    Manual publish/subscribe.

--- meteor-multidraw-demo.js
+++ meteor-multidraw-demo.js
@@ -1,12 +1,20 @@
-points = new Meteor.Collection('points');
+points = new Meteor.Collection('pointsCollection');
 
 if (Meteor.isClient) {
   Template.drawingSurface.points = function () {
     return points.find();
   }
+
+  Deps.autorun( function () {
+    Meteor.subscribe('pointsSubscription');
+  });
 }
 
 if (Meteor.isServer) {
+  Meteor.publish('pointsSubscription', function () {
+    return points.find();
+  });
+
   Meteor.startup(function () {
     if (points.find().count() == 0) {
       points.insert({x: 100, y: 100});
